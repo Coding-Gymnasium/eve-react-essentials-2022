03fe6917e46db6b68aeb1808db58eb52a555ab77
Nico Rithner <nicorithner@gmail.com>
2022-09-21 03:41:55 -0600

    Experiment with destructuring to access properties

--- exercise-files/Ch04/04_01/start/react-app/src/App.js
+++ exercise-files/Ch04/04_01/start/react-app/src/App.js
@@ -1,9 +1,15 @@
-import './App.css';
+import "./App.css";
 
-function App() {
+const cities = ["Tokyo", "Tahoe City", "Bend"];
+console.log(cities[0]);
+
+const [firstCity, secondCity] = ["Tokyo", "Tahoe City", "Bend"];
+console.log(secondCity);
+
+function App({ library }) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
