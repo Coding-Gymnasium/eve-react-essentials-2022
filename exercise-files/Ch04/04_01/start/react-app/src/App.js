06c5c1caf24d575a14970f425b581ad767ec9c81
Nico Rithner <nicorithner@gmail.com>
2022-09-21 04:09:00 -0600

    Handle input changes with useState and
    function onChange

--- exercise-files/Ch04/04_01/start/react-app/src/App.js
+++ exercise-files/Ch04/04_01/start/react-app/src/App.js
@@ -1,28 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState("happy");
-  const [secondary, setSecondary] = useState("tired");
-
-  useEffect(() => {
-    console.log(`It's ${emotion} right now`);
-  }, [emotion]);
-
-  useEffect(() => {
-    console.log(`It's ${secondary}`);
-  }, [secondary]);
+  const [checked, setChecked] = useState(false);
 
   return (
     <div className="App">
-      <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion("sad")}>Sad</button>
-      <button onClick={() => setEmotion("happy")}>Sad</button>
-      <button onClick={() => setEmotion("excited")}>Excited</button>
-
-      <h2>Current secondary emotion is {secondary}</h2>
-      <button onClick={() => setSecondary("tired")}>Tired</button>
-      <button onClick={() => setSecondary("grateful")}>Grateful</button>
+      <input
+        type="checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      <label>{checked ? 'Checked' : 'Not Checked' }</label>
     </div>
   );
 }
