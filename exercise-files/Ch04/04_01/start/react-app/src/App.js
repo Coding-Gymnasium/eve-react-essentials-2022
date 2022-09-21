46239c67286f05ae97b8a9af909a5ba1701544be
Nico Rithner <nicorithner@gmail.com>
2022-09-21 04:01:06 -0600

    Add useEffect. Explore use of dependency array

--- exercise-files/Ch04/04_01/start/react-app/src/App.js
+++ exercise-files/Ch04/04_01/start/react-app/src/App.js
@@ -1,14 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [emotion, setEmotion] = useState('happy');
+  const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary}`);
+  }, [secondary]);
 
   return (
     <div className="App">
       <h1>Current emotion is {emotion}</h1>
-      <button onClick={() => setEmotion('sad')}>Sad</button>
-      <button onClick={() => setEmotion('excited')}>Excited</button>
+      <button onClick={() => setEmotion("sad")}>Sad</button>
+      <button onClick={() => setEmotion("happy")}>Sad</button>
+      <button onClick={() => setEmotion("excited")}>Excited</button>
+
+      <h2>Current secondary emotion is {secondary}</h2>
+      <button onClick={() => setSecondary("tired")}>Tired</button>
+      <button onClick={() => setSecondary("grateful")}>Grateful</button>
     </div>
   );
 }
