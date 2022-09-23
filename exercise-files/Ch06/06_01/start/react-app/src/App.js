8a5bf4c4d07cf77c9ef6d5fe26cc3ba0d3529897
Nico Rithner <nicorithner@gmail.com>
2022-09-23 17:13:35 -0600

    Add loading and Error state

--- exercise-files/Ch06/06_01/start/react-app/src/App.js
+++ exercise-files/Ch06/06_01/start/react-app/src/App.js
@@ -1,28 +1,43 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 
-function GithubUser({ name, location, avatar}) {
+function GithubUser({ name, location, avatar }) {
   return (
     <div>
       <h1>{name}</h1>
       <h2>{location}</h2>
-      <img src={avatar} height={150} alt={name}/>
+      <img src={avatar} height={150} alt={name} />
     </div>
   );
 }
 
 function App() {
   const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch(`https://api.github.com/users/nicorithner`)
       .then((response) => response.json())
-      .then(setData(data));
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
   }, []);
-  if (data) {
-    return <GithubUser name={data.name} location={data.location} avatar={data.avatar_url} />;
+
+  if (loading) return <h1>Loading ...</h1>;
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>;
   }
-  return <h1>Data</h1>;
+  if (!data) return null;
+  return (
+    <GithubUser
+      name={data.name}
+      location={data.location}
+      avatar={data.avatar_url}
+    />
+  );
 }
 
 export default App;
