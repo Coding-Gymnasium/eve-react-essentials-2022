f73d42bfba3eb807698df9d03ff920664c4d0279
Nico Rithner <nicorithner@gmail.com>
2022-09-24 08:40:07 -0600

    Add links

--- exercise-files/Ch07/07_01/start/react-app/src/App.js
+++ exercise-files/Ch07/07_01/start/react-app/src/App.js
@@ -1,8 +1,21 @@
 import "./App.css";
+import { Link } from "react-router-dom";
+function Navigation() {
+  return (
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </div>
+  );
+}
 
 function Home() {
   return (
     <div>
+      <Navigation />
       <h1>My Website</h1>
     </div>
   );
@@ -11,6 +24,7 @@ function Home() {
 export function About() {
   return (
     <div>
+      <Navigation />
       <h1>About Us</h1>
     </div>
   );
@@ -19,6 +33,7 @@ export function About() {
 export function Contact() {
   return (
     <div>
+      <Navigation />
       <h1>Contact Us</h1>
     </div>
   );
